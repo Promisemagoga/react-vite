6e59e7a3f93528ba100cecab465b5c75a32f46d9
unknown <tshegofatsopromise362@gmail.com>
2025-07-30 11:18:18 +0200

    updated

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { MainContainer } from "../MainContainer";
+import { PageContainer } from "../PageContainer";
+import { HeroSection } from "../components/HeroSection/HeroSection";
+
+export const Contact = () => {
+  return (
+    <MainContainer>
+      <PageContainer>
+        <div style={{ marginTop: "100px" }}>
+          <HeroSection
+            img="/src/assets/smartphone.png"
+            imageStyle={{ borderRadius: 0 }}
+            subtitle="Give us a call"
+            description="012 345 6789"
+          />
+        </div>
+      </PageContainer>
+    </MainContainer>
+  );
+};
